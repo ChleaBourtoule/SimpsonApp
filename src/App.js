88f2242528760d2f46c03904d8bb46a75e227f8b
ChleaBourtoule <chlea.bourtoule@gmail.com>
2021-10-25 10:06:02 +0200

    simpsonApp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import DisplaySimpson from './components/DisplaySimpson';
+import { useState } from 'react';
+
+const sampleSimpson = {
+  quote:"I'm sleeping in the bath tub.",
+  character:"Marge Simpson",
+  image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMargeSimpson.png?1497567512205",
+  characterDirection:"Right"
+};
 
 function App() {
+  const [simpson, setSimpson] = useState(sampleSimpson);
+
+  const getSimpson = () => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+        setSimpson(data[0]);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DisplaySimpson simpson={simpson} />
+        <button type='button' onClick={getSimpson}>Get Simpson's Quote</button>
       </header>
     </div>
   );
